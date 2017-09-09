3ac4f4706deb7283ffb4542ffd5a4426149c3677
abhijithnraj <abhijithnrajg@gmail.com>
2017-09-09 07:39:21 +0530

    completed the bot for lumberjack

---
+++ game-bot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import time 
+import pyautogui
+import numpy as np
+def get_image(im):
+    return im.crop((820, 600, im.width-800,im.height-400))
+
+def action(key):
+    pyautogui.press(key)
+    pyautogui.press(key)
+
+n=int(input("Enter the score:\n"))/2
+n=int(n/2)
+time.sleep(5)
+flag=0
+for i in range(n):
+    im = pyautogui.screenshot()
+    c=get_image(im)
+    left=c.crop((0,0,c.width/2,c.height))
+    right=c.crop((c.width/2,0,c.width,c.height))
+    l_val=np.sum(np.array(left.getpixel((left.width/2,left.height/2))))
+    r_val=np.sum(np.array(right.getpixel((right.width/2,right.height/2))))
+
+    if(l_val<r_val):
+            print("tree at left")
+            action("right")
+    else:
+            print("tree at right")
+            action("left")
+
+    time.sleep(0.15)
